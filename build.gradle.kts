b70d45da112b8aea11b5cd32a00b722b58baf2ba
yarasoftes <yaraasx1@gmail.com>
2026-04-09 11:55:09 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,5 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-//import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-//import kotlin.text.set
-
 plugins {
     kotlin("jvm") version "2.2.21"
     kotlin("plugin.serialization") version "2.2.21"
@@ -17,15 +14,10 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.10.0")
     implementation("de.fabmax.kool:kool-core:0.19.0")
     implementation("de.fabmax.kool:kool-physics:0.19.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
-    // dependencies - блок с подключенными бмблиотеками (зависимости)
-    // implementation - команда добавлябщая библиотеку в проект
-    // org.jetbrains.kotlinx:...  - коорлинаты библиотеки
-    // kotlinx-coroutines-core - имя необходимого артефакта
-    // версия библиотеки 1.7.3
 }
 
 tasks.test {
@@ -34,10 +26,6 @@ tasks.test {
 kotlin {
     jvmToolchain(17)
 }
-//val compileKotlin: KotlinCompile by tasks
-//compileKotlin.compilerOptions {
-//    freeCompilerArgs.set(listOf("-Xnested-type-aliases"))
-//}
 val compileKotlin: KotlinCompile by tasks
 compileKotlin.compilerOptions {
     freeCompilerArgs.set(listOf("-Xnested-type-aliases"))
